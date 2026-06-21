7bfaa42ff044a44023e7424fbd85da3015181815
franzageek <franzageek2020@gmail.com>
2026-06-21 22:45:15 +0200

    Split evec code into header and source

--- include/evec.h
+++ include/evec.h
@@ -47,105 +47,12 @@ evec_t evec__stack_new(u16 size_bytes)
 }
 */
 
-void evec__free(evec_t* evec)
-{
-    free(evec->data);
-    memset(evec, 0, sizeof(evec_t));
-    free(evec);
-    return;
-}
+void evec__free(evec_t* evec);
 
-evec_t* evec__new(u16 size_bytes)
-{
-    evec_t* evec = calloc(1, sizeof(evec_t));
-    if (!evec)
-    {
-        perror("evec: calloc:");
-        exit(EXIT_FAILURE);
-    }
+evec_t* evec__new(u16 size_bytes);
 
-    u8* data = calloc(8, size_bytes);
-    if (!data)
-    {
-        perror("evec: calloc:");
-        exit(EXIT_FAILURE);
-    }
+void evec__push(evec_t* evec, void* data);
 
-    evec->data = data;
-    evec->capacity = 8;
-    evec->size = 0;
-    evec->item_size = size_bytes;
-    evec->valid = true;
-    data = NULL;
-    return evec;
-}
-
-void evec__push(evec_t* evec, void* data)
-{
-    if (evec->valid)
-    {
-        if (evec->size >= evec->capacity)
-        {
-            u8* new_data = (u8*)calloc(evec->capacity + (evec->capacity / 2), evec->item_size);
-            if (!new_data)
-            {
-                perror("evec: calloc:");
-                evec__free(evec);
-                exit(EXIT_FAILURE);
-            }
-            
-            memmove(new_data, evec->data, evec->capacity * evec->item_size);
-            if (memcmp(new_data, evec->data, evec->capacity * evec->item_size) != 0)
-            {
-                fprintf(stderr, "evec: an error occurred after memmove() call: source and destination do not match\n");
-                evec__free(evec);
-                exit(EXIT_FAILURE);
-            }
-    
-            free(evec->data);
-            evec->data = new_data;
-            new_data = NULL;
-            evec->capacity += evec->capacity / 2;
-        }
-
-        u8* dest = &evec->data[evec->size * evec->item_size];
-        memmove(dest, data, evec->item_size);
-        if (memcmp(data, dest, evec->item_size) != 0)
-        {
-            fprintf(stderr, "evec: an error occurred after memmove() call: source and destination do not match\n");
-            evec__free(evec);
-            exit(EXIT_FAILURE);
-        }
-        
-        evec->size += 1;
-        return;
-    }
-    else
-    {
-        fprintf(stderr, "evec: an error occurred after evec__push(): called on an invalid evec\n");
-        exit(EXIT_FAILURE);
-    }    
-}
-
-void* evec__at(evec_t* evec, u16 index)
-{
-    if (evec->valid)
-    {
-        if (index < evec->size)
-            return (void*)&evec->data[index * evec->item_size];
-        
-        else
-        {
-            fprintf(stderr, "evec: an error occurred after evec__at(): attempt to read out of range\n");
-            evec__free(evec);
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        fprintf(stderr, "evec: an error occurred after evec__at(): called on an invalid evec\n");
-        exit(EXIT_FAILURE);
-    }
-}
+void* evec__at(evec_t* evec, u16 index);
 
 #endif
